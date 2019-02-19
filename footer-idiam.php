ce31b793068903596bd5530f25d4167dd98b7921
Lyngall <soporte@esbrillante.com>
2019-02-19 12:03:36 -0600

    last changes

--- footer-idiam.php
+++ footer-idiam.php
@@ -2,32 +2,6 @@
 
 
 
-<?php
-global $mk_options;
-
-$mk_footer_class = $show_footer = $disable_mobile = $footer_status = '';
-
-$post_id = global_get_post_id();
-if ( $post_id ) {
-	$show_footer = get_post_meta( $post_id, '_template', true );
-	$cases = array( 'no-footer', 'no-header-footer', 'no-header-title-footer', 'no-footer-title' );
-	$footer_status = in_array( $show_footer, $cases );
-}
-
-if ( $mk_options['disable_footer'] == 'false' || ( $footer_status ) ) {
-	$mk_footer_class .= ' mk-footer-disable';
-}
-
-if ( $mk_options['footer_type'] == '2' ) {
-	$mk_footer_class .= ' mk-footer-unfold';
-}
-
-
-$boxed_footer = (isset( $mk_options['boxed_footer'] ) && ! empty( $mk_options['boxed_footer'] )) ? $mk_options['boxed_footer'] : 'true';
-$footer_grid_status = ($boxed_footer == 'true') ? ' mk-grid' : ' fullwidth-footer';
-$disable_mobile = ($mk_options['footer_disable_mobile'] == 'true' ) ? $mk_footer_class .= ' disable-on-mobile' : ' ';
-
-?>
 
 <section id="mk-footer-unfold-spacer">
     
@@ -151,25 +125,7 @@ $disable_mobile = ($mk_options['footer_disable_mobile'] == 'true' ) ? $mk_footer
     
 </section>
 
-<section id="mk-footer" class="<?php echo $mk_footer_class; ?>" <?php echo get_schema_markup( 'footer' ); ?>>
-	<?php if ( $mk_options['disable_footer'] == 'true' && ! $footer_status ) : ?>
-	<div class="footer-wrapper<?php echo $footer_grid_status; ?>">
-		<div class="mk-padding-wrapper">
-			<?php mk_get_view( 'footer', 'widgets' ); ?>
-			<div class="clearboth"></div>
-		</div>
-	</div>
-	<?php endif; ?>
-	<?php
-	if ( $mk_options['disable_sub_footer'] == 'true' && ! $footer_status ) {
-		mk_get_view(
-			'footer', 'sub-footer', false, [
-				'footer_grid_status' => $footer_grid_status,
-			]
-		);
-	}
-?>
-</section>
+
 </div>
 <?php
 	global $is_header_shortcode_added;
